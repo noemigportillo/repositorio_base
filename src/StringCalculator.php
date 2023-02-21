cbd8f1f7d536444ac09a7395cd84e4f1d3f3f357
noemi <gonzalez.131620@e.unavarra.es>
2023-02-21 20:14:50 +0100

    Funcionalidades 1,2 y 3

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -5,4 +5,22 @@ namespace Deg540\StringCalculatorPHP;
 class StringCalculator
 {
     // TODO: String Calculator Kata
+    public function add(string $numbers_to_add): int{
+        if(empty($numbers_to_add)){
+            return 0;
+        }
+
+        if(str_contains($numbers_to_add, "\n") || str_contains($numbers_to_add, ",")){
+            $delimiters = [",", "\n"];
+            $unified_numbers_to_add = str_replace($delimiters, $delimiters[0], $numbers_to_add);
+            $numbers_sum = 0;
+            $separated_numbers = explode($delimiters[0], $unified_numbers_to_add);
+            for($number_position = 0; $number_position < count($separated_numbers); $number_position++){
+                $numbers_sum += (int)$separated_numbers[$number_position];
+            }
+            return $numbers_sum;
+        }
+
+        return (int)$numbers_to_add;
+    }
 }
--- tests/StringCalculatorTest.php
+++ tests/StringCalculatorTest.php
@@ -10,4 +10,39 @@ use PHPUnit\Framework\TestCase;
 final class StringCalculatorTest extends TestCase
 {
     // TODO: String Calculator Kata Tests
+    public function test_0_if_string_empty()
+    {
+        $stringCalculator = new StringCalculator();
+
+        $sumResult = $stringCalculator->add("");
+
+        $this->assertEquals(0, $sumResult);
+    }
+
+    public function test_one_number_string_returns_its_value()
+    {
+        $stringCalculator = new StringCalculator();
+
+        $sumResult = $stringCalculator->add("1");
+
+        $this->assertEquals(1, $sumResult);
+    }
+
+    public function test_more_than_one_number_string_returns_the_sum()
+    {
+        $stringCalculator = new StringCalculator();
+
+        $sumResult = $stringCalculator->add("1,2,3");
+
+        $this->assertEquals(6, $sumResult);
+    }
+
+    public function test_add_endures_line_break_and_comma_as_delimiter_in_numbers_string()
+    {
+        $stringCalculator = new StringCalculator();
+
+        $sumResult = $stringCalculator->add("1\n2,3");
+
+        $this->assertEquals(6, $sumResult);
+    }
 }
